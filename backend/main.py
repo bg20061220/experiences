dba3e2abfa95024553c337b1994daf1c00a0fea1
bg20061220 <goel.bhavya020@gmail.com>
2026-02-06 00:52:31 -0500

    add main url

--- backend/main.py
+++ backend/main.py
@@ -12,9 +12,10 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://resumetailorai.vercel.app",
         "https://resumetailorai-9pzkpka42-bhavya-goels-projects.vercel.app",
     ],
-    allow_origin_regex=r"https://.*bhavya-goels-projects\.vercel\.app",
+    allow_origin_regex=r"https://resumetailorai.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
